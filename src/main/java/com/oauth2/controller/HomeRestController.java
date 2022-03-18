d368bb26db2ee09cb6c88a7b44062f504e9984c9
Tiago Nagy <tiagonagy@hotmail.com>
2022-03-18 18:23:57 -0300

    initial commit

---
+++ src/main/java/com/oauth2/controller/HomeRestController.java
@@ -0,0 +1,12 @@
+package com.oauth2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeRestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
